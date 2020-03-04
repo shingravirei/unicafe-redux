107c56a907144e5f5e7f2f0ee35e21835b843589
Victor <zedfromionia@gmail.com>
2020-03-04 16:50:58 +0000

    fixed some logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
 
-function App() {
+const App = ({ store }) => {
     return (
         <div>
-            <h1>Hello, World!</h1>
+            <h1>Give Feedback</h1>
+            <div>
+                <button
+                    onClick={() => {
+                        store.dispatch({ type: 'GOOD' });
+                    }}
+                >
+                    good
+                </button>
+                <button
+                    onClick={() => {
+                        store.dispatch({ type: 'OK' });
+                    }}
+                >
+                    neutral
+                </button>
+                <button
+                    onClick={() => {
+                        store.dispatch({ type: 'BAD' });
+                    }}
+                >
+                    bad
+                </button>
+            </div>
+
+            <h2>Statistics</h2>
+            <p>Good {store.getState().good}</p>
+            <p>Neutral {store.getState().ok}</p>
+            <p>Bad {store.getState().bad}</p>
         </div>
     );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import counterReducer from './reducers/reducer';
+
+const store = createStore(counterReducer);
+
+const renderApp = () => {
+    ReactDOM.render(<App store={store} />, document.getElementById('root'));
+};
+
+renderApp();
+
+store.subscribe(() => {
+    renderApp();
+});
